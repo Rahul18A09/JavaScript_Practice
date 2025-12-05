2fcbdbb379f463bcf989fe540e5595307c655e1d
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-05 10:32:34 +0530

    update

---
+++ Day1- BackTick Logic/script.js
@@ -0,0 +1,23 @@
+// let name = "Rahul";
+// let message = `Hello, ${name}`;
+// console.log(message);
+
+// Example 1 — Display Dynamic Text in HTML
+
+// let firstName = "Rahul";
+// let lastName = "Bharada";
+
+// document.getElementById("greet").innerHTML = `Hello, ${firstName} ${lastName}. WelCome to the Coding World.!`;
+
+// Example 2 — Add Multiple Lines of HTML Dynamically
+
+let title = "My Profile";
+let role = "Frontend Developer";
+
+document.getElementById("card").innerHTML = `
+ <h2>${title}</h2>
+ <p>${role}</p>
+ <button>Contact Me</button>
+ `;
+
+ 
