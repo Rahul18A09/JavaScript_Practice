325588387b099a810004b46dd77bf0e1ea2c76c8
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-08 23:37:44 +0530

    update

---
+++ Day3-Run JS, variables, types, console.log/script1.js
@@ -0,0 +1,22 @@
+// console.log(true && false);
+// console.log(false || true);
+// console.log(!false);
+// console.log(5 > 3 && 2 > 5);
+// console.log(10 === 10)||(10 === "10");
+
+// Ternary Operator Pralet
+
+// let age = 16;
+// let message = age >= 18 ? "Allowed" : "Not Allowed";
+// console.log(message);
+
+// let score = 75;
+// let result = score > 80 ? "Ecellent" : "Good";
+// console.log(result);
+
+// let isMember = false;
+// let discount = isMember ? 30 : 10;
+// console.log(discount);
+
+
+
