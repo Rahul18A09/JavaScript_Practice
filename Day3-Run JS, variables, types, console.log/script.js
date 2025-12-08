eb9279d8134b664ca0e4c94c2fcd8362e9052438
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-08 17:16:39 +0530

    update

---
+++ Day3-Run JS, variables, types, console.log/script.js
@@ -0,0 +1,29 @@
+console.log("Hello, Javascript!");
+
+// Declare variables:
+
+// modern way
+//let age = 28;   //It can reassign but cannot redeclare
+//const name = "Rahul"; //It cannot reassign and redeclare
+// var a = 25;
+// console.log(a);
+
+
+// typeof 123  //number
+// typeof "Rahul" //string
+// typeof true //boolean
+// typeof null //object
+// typeof undefined // undefined
+// 
+// console.log(typeof undefined);
+
+// Type of Operators
+
+//1.Arithmetic Operators
+//2.Assignment Operators
+//3.Comparison Operators
+//4.Logical Operators
+//5.String Operators
+//6.Ternary Operators
+//7.Typeof Operators
+//8.Bitwise Operators
