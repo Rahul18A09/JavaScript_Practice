147c45bdc26ca31471697ec1eb399f05ecd6b763
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-09 17:29:58 +0530

    new

---
+++ Day4-Control Flow & Loops/script.js
@@ -0,0 +1,32 @@
+// 1. if / else
+
+    // let age = 20;
+
+    // if(age >= 18) {
+    //     console.log("You are an Adult!");
+        
+    // }else {
+    //     console.log("You are minor");    
+    // }
+
+
+    // if / else if / else
+
+        // let marks = 85;
+
+        // if(marks >= 90){
+        //     console.log("Grade A");
+
+        // }else if (marks >= 80){
+        //     console.log("Grade B");
+            
+        // }else if (marks >= 70){
+        //     console.log("Grade C");
+            
+        // }else {
+        //     console.log("Grade D");
+           
+        // }
+
+        
+    
