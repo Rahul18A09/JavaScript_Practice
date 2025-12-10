7ed4a85fae17413153b53f0bf9663fc81486fc7e
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-10 17:19:33 +0530

    update

--- Day4-Control Flow & Loops/script1.js
+++ Day4-Control Flow & Loops/script1.js
@@ -3,7 +3,7 @@
 // let i = 1;
 
 // for (i = 1; i <= 20; i++){
-//     if (i % 2 === 0 || i != 10);
+//     if (i % 2 === 0 && i != 10);
 
 //     console.log(i);
     
@@ -11,11 +11,66 @@
 
 // Example 2: Print numbers that are either divisible by 3 OR 5
 
-let i  = 1;
+// let i  = 1;
 
-for (i = 1; i <= 30 ; i++){
-    if(i  % 3 === 0 || i % 5 === 0){
-        console.log(i);
+// for (i = 1; i <= 30 ; i++){
+//     if(i  % 3 === 0 || i % 5 === 0){
+//         console.log(i);
         
-    }
-}
+//     }
+// }
+
+// Example 3: Skip negative numbers using NOT (!)
+
+// let numbers = [12, -4, 0, 7, -2, 14];
+
+// for (let num of numbers){
+//     if(!(num < 0)){
+//         console.log(num);
+        
+//     }
+// }
+
+// Example 4: Find students with marks > 60 AND attendance > 75%
+
+// let students  = [
+//     { name: "Amit", marks: 85, attendance: 80 },
+//     { name: "Riya", marks: 55, attendance: 90 },
+//     { name: "Sam",  marks: 70, attendance: 60 },
+//     { name: "Neha", marks: 92, attendance: 88 }
+// ];
+
+// for (let student of students){
+//     if(student.marks >= 60 && student.attendance >= 75){
+//         console.log(student.name);
+        
+//     }
+// }
+
+// Example 5: Stop loop when a number between 50 and 60 is found
+
+//  let nums = [10, 22, 35, 48, 53, 55, 58, 70];
+
+//  for (let num of nums){
+//     if (num >= 50 && num <= 60){
+//         console.log(num);
+        
+//     }
+//  }
+
+// Example 6: Show only users aged 18 OR with admin role
+
+// let users = [
+//     { name: "Rahul", age: 16, role: "member" },
+//     { name: "Aarav", age: 22, role: "member" },
+//     { name: "Sana",  age: 17, role: "admin" },
+// ];
+
+// for (let user of users){
+
+//     if(user.age > 18 || user.role === "admin"){
+//         console.log(user.name);
+        
+//     }
+// }
+
