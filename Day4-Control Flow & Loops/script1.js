36b8fdfa0a3a6d89ee6615a4051949487075c66f
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-10 10:29:58 +0530

    new

--- Day4-Control Flow & Loops/script1.js
+++ Day4-Control Flow & Loops/script1.js
@@ -1,1 +1,21 @@
 // 1.Example 1: Print only even numbers (comparison + logical operator)
+
+// let i = 1;
+
+// for (i = 1; i <= 20; i++){
+//     if (i % 2 === 0 || i != 10);
+
+//     console.log(i);
+    
+// }
+
+// Example 2: Print numbers that are either divisible by 3 OR 5
+
+let i  = 1;
+
+for (i = 1; i <= 30 ; i++){
+    if(i  % 3 === 0 || i % 5 === 0){
+        console.log(i);
+        
+    }
+}
