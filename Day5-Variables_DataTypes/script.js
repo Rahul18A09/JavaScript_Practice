155ce51a4e5bf05e4e5a10c747541b37694e142b
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-18 17:25:13 +0530

    new

---
+++ Day5-Variables_DataTypes/script.js
@@ -0,0 +1,45 @@
+// let message;
+// message = "Hello";
+// console.log(message);
+// alert(message);
+
+
+// let user = "Rahul";
+// let age = 25;
+// let message = "Hello";
+
+
+// let message;
+// message = "Hello";
+// message = "World";
+// console.log(message);
+
+
+// let hello = "Hello World";
+// let message;
+// message = hello;
+// alert(hello);
+// alert(message);
+
+
+// Variables naming
+
+// let username;
+// let test123;
+// let user_name;
+// let $name;
+// let $name1 = "Rahul";
+// alert($name1);
+
+// let let = 5;  it is not allowable
+// let return  = 5; it is not allowable
+
+// let name = "Abdul";
+
+// let admin;
+
+// admin = name;
+
+// alert(admin);
+
+// alert("I am not a robot");
