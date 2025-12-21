b9248e7949db56cce8e2deff3cafcfaf5db482ec
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-21 18:32:36 +0530

    new

---
+++ Day5-Variables_DataTypes/datatype.js
@@ -0,0 +1,60 @@
+// alert(NaN + 1);
+// alert(3 * NaN);
+
+// let name = "rahul";
+
+// alert(`Hello, ${name}`);
+// alert(`The result is ${1 + 3}`);
+
+// let isStudent = true;
+// let isRight = false;
+
+// alert(isStudent);
+// alert(isRight);
+
+// let isGreater = 4 > 1;
+// alert(isGreater);
+
+// let age = null;
+// alert(age);
+
+// let name;
+// alert(name);
+
+// alert(typeof 1);
+// alert(typeof "Rahul");
+// alert(typeof n10);
+// alert(typeof true);
+// alert(typeof Symbol('id'));
+// alert(typeof Math);
+// alert(typeof alert);
+
+
+// let age = prompt("How ald are you", 100);
+// alert(`You are ${age} years old.`);
+
+// let isBoss = confirm('Are you the boss?');
+// alert(isBoss);
+
+// alert()
+// prompt()
+// confirm()
+
+// let str = "123";
+// alert(Number(str));
+// alert(str);
+
+// alert(Boolean(1));
+// alert(Boolean(0));
+// alert(Boolean("hello"));
+// alert(Boolean(""));
+
+let userName = "Abdul";
+ 
+alert( `hello ${1}` ); // ?
+ 
+alert( `hello ${"userName"}` ); // ?
+ 
+alert( `hello ${userName}` ); // ?
+
+
