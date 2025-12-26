e30c455db377d092cafd576bdb77a4670465e975
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-26 11:22:41 +0530

    update

---
+++ Day6- Function, Scope & Arrow Function/script.js
@@ -0,0 +1,51 @@
+// let year = prompt("In Which year India got Indepandent?", "");
+// if ( year  == 1947){
+//     alert("That is Correct!");
+//     alert("You are So smart!");
+// }
+
+// let company = prompt("Which company  created javascript?", " ");
+// (company == "Netscape")
+//  ? alert("Right!") : alert("Wrong.");
+
+// let company = prompt("Which company  created javascript?", "");
+
+// if (company == "Netscape"){
+//     alert ("Right!");
+// }else {
+//     alert("wrong!");
+// }
+
+// function add (a ,b){
+// console.log(a + b);
+
+// }
+// add (2, 3);
+// add (10 ,20);
+
+// function with parameter
+
+// function greetuser(name){
+//     console.log(`Hello ${name}`);
+
+// }
+
+// greetuser ("Rahul");
+// greetuser ("Asha");
+
+// function with return
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// let result = add (10, 15);
+// console.log(result);
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+let output = multiply(5, 6);
+console.log(output);
+console.log(multiply(5, 4));
