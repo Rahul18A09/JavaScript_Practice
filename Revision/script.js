86260795caf876010ea739d0dfb3de4d40b4f653
Rahul18A09 <rahulbharda60@gmail.com>
2026-01-17 18:46:54 +0530

    new

---
+++ Revision/script.js
@@ -0,0 +1,51 @@
+// var name = "Rahul";
+// var name = "Asha";
+ // re-declared
+// console.log(name);
+
+// let age = 25;
+// age = 26;
+// console.log(age);
+
+// {
+//     let age = 30;
+//     console.log(age);
+    
+// }
+
+// const x = 10;
+// x= 20;
+// console.log(x);
+
+// const user =  {name: "Rahul"};
+// console.log(user.name);
+
+// user.name = "Asha";
+// console.log(user.name);
+
+// const arr = [1,2,3];
+// arr.push(4,5,6);
+// console.log(arr);
+
+// let name = "Rahul";
+// console.log(name);
+
+// let age = 26;
+// console.log(age);
+
+// let isDev = true;
+// console.log(isDev);
+
+// let x;
+// console.log(x);
+
+// let y = null ;
+// console.log(y);
+
+console.log(typeof null);
+
+
+
+
+
+
