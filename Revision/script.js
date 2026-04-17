c1047e4112eac58f8198b28a81abdba448bc4537
Rahul Bharada <147308572+Rahul18A09@users.noreply.github.com>
2026-04-17 23:41:21 +0530

    Example

--- Revision/script.js
+++ Revision/script.js
@@ -1,4 +1,4 @@
-ee
+inee
 
 // var name = "Rahul";
 // var name = "Asha";
@@ -608,3 +608,17 @@ setInterval
 DOM events
 4️⃣ Event Loop
 Moves tasks from queues → Call Stack when stack is empty
+
+Example 
+
+console.log("Start");
+
+setTimeout(() => {
+  console.log("Timeout");
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log("Promise");
+});
+
+console.log("End");
